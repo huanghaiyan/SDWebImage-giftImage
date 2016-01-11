e1ec862069dda380642cc72ef2ad5033fac5d8ba
huanghy <qq1531398577@126.com>
2016-01-11 17:31:30 +0800

    使用图片名加载gif

--- SDWebImage-giftImage/ViewController.m
+++ SDWebImage-giftImage/ViewController.m
@@ -25,21 +25,23 @@
 - (void)initLoadingImageView
 
 {
+
     NSString  *filePath = [[NSBundle bundleWithPath:[[NSBundle mainBundle] bundlePath]] pathForResource:@"2.gif" ofType:nil];
     
     NSData  *imageData = [NSData dataWithContentsOfFile:filePath];
-    
     
     self.loadingImageView = [[UIImageView alloc]init];
         self.loadingImageView.frame = CGRectMake(0, 0, self.view.frame.size.width,  self.view.frame.size.height);
     
     self.loadingImageView.backgroundColor = [UIColor blueColor];
-    
+    //第一种方法使用imageData加载
     self.loadingImageView.image = [UIImage sd_animatedGIFWithData:imageData];
+    //第二种方法使用图片名字加载
+    //self.loadingImageView.image = [UIImage sd_animatedGIFNamed:@"2"];
     
     [self.view addSubview:self.loadingImageView];
     
-    //[self.view bringSubviewToFront:self.loadingImageView];
+    [self.view bringSubviewToFront:self.loadingImageView];
     
 }
 
